d6dead9f3f0bbe5887077aeb5290f3f27cb2dae5
taylormarz <taylormartind@gmail.com>
2024-11-28 14:01:58 -0500

    backend setup for server

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+// COMP3123 - Fullstack Development, Labtest 02
+// Taylor Martin, Student ID: 100849882
+
+import { express } from 'express';
+import { cors } from 'cors';
+
+const app = express();
+const SERVER_PORT = 8080;
+
+// enabling communication with frontend
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET'],
+    credentials: true
+}));
+
+// processing requests
+app.use(express.json());
+
+app.listen(SERVER_PORT, () => { 
+    console.log(`Server running on http://localhost:${SERVER_PORT}`) 
+});
